7e18ed2f0d7b66f0f85dcf91502019485f458081
Sameera Mallawaarachchi <dmsameeram@gmail.com>
2024-08-01 21:51:12 +0530

    fixes

--- sketch.js
+++ sketch.js
@@ -1,50 +1,61 @@
 let startD = false;
 let drawX;
 let drawY;
-let drawR, drawG, drawB, drawT, drawS;
+let drawR, drawG, drawB, drawT, drawS, drawST;
 let type = 0;
 let ifStroke;
+let ifRotate;
 
 function setup() {
+  startD = false;
   ifStroke = random(0, 10)
+  ifRotate = random(0, 10)
   type = round(random(0, 2));
   createCanvas(windowWidth, windowHeight);
+  translate(windowWidth / 2, windowHeight / 2);
+
   background(30);
   frameRate(60);
   drawX = windowWidth / 2;
   drawY = windowHeight / 2;
-  drawR = drawG = drawB = drawT = 100;
+  drawR = drawG = drawB = drawT = drawST = 100;
   drawS = getS();
   setTimeout(() => {
     startD = true;
   }, 1500);
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(30);
 }
 function mousePressed() {
   let fs = fullscreen();
   fullscreen(!fs);
   setup();
 }
+
 function draw() {
 
   if (startD) {
-    console.log(frameRate())
+
+    drawR = insideCol(drawR + getCol());
+    drawG = insideCol(drawG + getCol());
+    drawB = insideCol(drawB + getCol());
+    drawST = insideCol(drawST + getCol());
+    drawT = getT();
+    drawS = getS();
 
     if (ifStroke > 5) {
-      stroke(drawR, drawG, drawB, 255);
+      stroke(drawST);
     } else {
       noStroke();
 
     }
-    drawR = insideCol(drawR + getCol());
-    drawG = insideCol(drawG + getCol());
-    drawB = insideCol(drawB + getCol());
-    drawT = getT();
-    drawS = getS();
-
-
     fill(drawR, drawG, drawB, drawT);
 
-    //rotate(getR());
+    if (ifRotate > 5) {
+      rotate(getR());
+    }
 
     drawX = drawX + getX();
     drawY = drawY + getY();
@@ -101,22 +112,22 @@ function insideY(cord) {
 }
 
 function getX() {
-  return (round(random(-6, 6)) * drawS) / 3;
+  return (round(random(-4, 4)) * drawS) / 2;
 }
 
 function getY() {
-  return (round(random(-6, 6)) * drawS) / 3;
+  return (round(random(-4, 4)) * drawS) / 2;
 }
 function getS() {
   return round(random(2, 4)) * 10;
 }
 
 function getCol() {
-  return round(random(-5, 5)) * 5;
+  return round(random(-5, 5)) * 2;
 }
 function getT() {
   return round(random(1, 25)) * 10;
 }
 function getR() {
-  return round(random(-5, 5)) * 10;
+  return round(random(-5, 5)) * 5;
 }
